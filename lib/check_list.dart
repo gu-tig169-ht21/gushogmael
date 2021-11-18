1d4a3d634f85083590252229610844f23fb59c65
elinhm <gushogmael@student.gu.se>
2021-11-18 15:39:02 +0100

    steg2

---
+++ lib/check_list.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '/model.dart';
+
+class CheckList extends StatelessWidget {
+  List<CheckBoxState> list;
+
+  CheckList(this.list);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+        children:
+            list.map((checkbox) => _buildCheckbox(context, checkbox)).toList());
+  }
+
+  Widget _buildCheckbox(context, CheckBoxState checkbox) => CheckboxListTile(
+        contentPadding: const EdgeInsets.all(12.0),
+        controlAffinity: ListTileControlAffinity.leading,
+        title: Text(checkbox.title,
+            style: TextStyle(
+                decoration:
+                    checkbox.value ? TextDecoration.lineThrough : null)),
+        onChanged: (value) {
+          var state = Provider.of<MyState>(context, listen: false);
+          state.setDoneCheckList(checkbox, false);
+        },
+        value: checkbox.value,
+        secondary: IconButton(
+            icon: const Icon(Icons.clear),
+            onPressed: () {
+              Provider.of<MyState>(context, listen: false)
+                  .removeBox(checkbox); //ta bort
+            }),
+      );
+}
